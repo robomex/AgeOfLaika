22ca2334bdc700642d391df7c50d42dd44d4f22c
robomex <atmorales@gmail.com>
2014-09-27 01:07:36 -0500

    age of laika challenge part 1 completed

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    @IBOutlet weak var humanYearsTextField: UITextField!
+    @IBOutlet weak var convertButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        dogYearsLabel.text = "\(humanYearsTextField.text.toInt()! * 7)" + " Dog Years"
+        dogYearsLabel.hidden = false
+        humanYearsTextField.text = ""
+        humanYearsTextField.resignFirstResponder()
+        convertButton.setTitle("Converted, Yo", forState: UIControlState.Normal)
+        convertButton.backgroundColor = UIColor.yellowColor()
+    }
 
 }
 
