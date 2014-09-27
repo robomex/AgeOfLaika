4125b44ce6855977a1292f9072ad2f4f6f08861f
robomex <atmorales@gmail.com>
2014-09-27 18:53:00 -0500

    through control flow challenge

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var dogYearsLabel: UILabel!
     @IBOutlet weak var humanYearsTextField: UITextField!
     @IBOutlet weak var convertButton: UIButton!
+    @IBOutlet weak var betterConvertButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,13 +26,29 @@ class ViewController: UIViewController {
     }
 
     @IBAction func convertButtonPressed(sender: UIButton) {
-        dogYearsLabel.text = "\(humanYearsTextField.text.toInt()! * 7)" + " Dog Years"
+        dogYearsLabel.text = "\(humanYearsTextField.text.toInt()! * 7)" + " Rough Dog Years"
         dogYearsLabel.hidden = false
         humanYearsTextField.text = ""
         humanYearsTextField.resignFirstResponder()
         convertButton.setTitle("Converted, Yo", forState: UIControlState.Normal)
         convertButton.backgroundColor = UIColor.yellowColor()
     }
+    
+    @IBAction func betterConvertButtonPressed(sender: UIButton) {
+        
+        if Double((humanYearsTextField.text as NSString).doubleValue) <= 2.0 {
+            dogYearsLabel.text = "\(Double((humanYearsTextField.text as NSString).doubleValue) * 10.5)" + " Dog Years"
+        } else {
+            dogYearsLabel.text = "\(21 + (Double((humanYearsTextField.text as NSString).doubleValue) - 2) * 4)" + " Dog Years"
+        }
+        
+        dogYearsLabel.hidden = false
+        humanYearsTextField.text = ""
+        humanYearsTextField.resignFirstResponder()
+        betterConvertButton.setTitle("Converted...Better, Yo", forState: UIControlState.Normal)
+        betterConvertButton.backgroundColor = UIColor.orangeColor()
+    }
+    
 
 }
 
